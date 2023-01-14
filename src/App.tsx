8e75198cd7df651c713033b4e41e777f2a8777c8
username <81845513+june3011@users.noreply.github.com>
2023-01-14 16:09:28 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
         <QueryClientProvider client={new QueryClient()}>
           {routing}
+          asdfasdf
           <Map />
         </QueryClientProvider>
       </BrowserRouter>
